698e3b12b5f49b3795459914bfef9ef99b2b82c7
kumakou1234 <72565166+kumakou1234@users.noreply.github.com>
2020-12-12 21:11:23 +0900

    ストロングパラメーターとバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         validates :nickname, presence: true
+         validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
+         validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
+         validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶ]+\z/}
+         validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶ]+\z/ }
+         validates :birthday, presence: true
 end
