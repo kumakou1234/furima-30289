6955916aa7758b21ee1925b4e9a55e78d1c776dd
kumakou1234 <72565166+kumakou1234@users.noreply.github.com>
2020-12-20 06:29:40 +0900

    itemモデルのバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,14 @@
 class Item < ApplicationRecord
+  validates :product_name, presence: true
+  validates :product_name_explanation, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :shipping_charges_id, presence: true
+  validates :prefectures_id, presence: true
+  validates :says_to_ship_id, presence: true
+  validates :price, presence: true
+  validates :image, presence: true
+
   has_one_attached :image
   belongs_to :user
 
