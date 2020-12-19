f9e8e255057f2ea7981fbcd4f0e9422c5e8f5b6f
kumakou1234 <72565166+kumakou1234@users.noreply.github.com>
2020-12-20 06:34:02 +0900

    itemテーブルのバリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,11 @@
 class Item < ApplicationRecord
   validates :product_name, presence: true
   validates :product_name_explanation, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :shipping_charges_id, presence: true
-  validates :prefectures_id, presence: true
-  validates :says_to_ship_id, presence: true
+  validates :category_id,                 numericality: { other_than: 1 }
+  validates :condition_id,                numericality: { other_than: 1 }
+  validates :shipping_charges_id,         numericality: { other_than: 1 }
+  validates :prefectures_id,              numericality: { other_than: 1 }
+  validates :says_to_ship_id,             numericality: { other_than: 1 }
   validates :price, presence: true
   validates :image, presence: true
 
