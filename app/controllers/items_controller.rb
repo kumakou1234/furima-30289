0ca8d098ce5fd38d47aeb5cb95f38092a0287235
kumakou1234 <72565166+kumakou1234@users.noreply.github.com>
2020-12-21 08:16:57 +0900

    データの保存確認

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,21 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
+  def new
+    @item = Item.new
+   
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+
+   private
+
+  def item_params
+     params.require(:item).permit(:image, :product_name, :product_name_explanation, :category_id, :condition_id, :shipping_charges_id, :prefectures_id, :says_to_ship_id, :price).merge(user_id: current_user.id)
+
+   end
+
 end
