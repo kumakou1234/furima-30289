2ca559f77fe7e660a25100ee5f0e56335b0f68b1
kumakou1234 <72565166+kumakou1234@users.noreply.github.com>
2020-12-27 16:14:42 +0900

    editページへのログイン権限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -23,6 +23,10 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+    end
   end
 
   def update
